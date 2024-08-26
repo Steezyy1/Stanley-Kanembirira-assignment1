ce82bf703c8391c85e46c6f97221d3b434081125
Steezyy1 <163055425+Steezyy1@users.noreply.github.com>
2024-08-26 21:22:18 +0200

    Add files via upload
    question 8 and 9

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the Bank class with account type "my initials" and balance regnumber
+        Bank bankAccount = new Bank("SLK", 230755);
+
+        // Invoke (call) the display method
+        bankAccount.display();
+    }
+}
